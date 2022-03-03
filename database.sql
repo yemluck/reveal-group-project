a38527044bbaeb8b5c9009f1283732266e6ca0b1
Chris Agbolade <yemluck@gmail.com>
2022-03-03 11:18:10 -0600

    reformatted database.sql, added comments table and classification table

--- database.sql
+++ database.sql
@@ -4,18 +4,18 @@
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
 
-
 CREATE TABLE "user" (
 	"id" serial NOT NULL,
 	"email_address" varchar(255) NOT NULL,
 	"password" varchar(255) NOT NULL,
-	"transparency" DECIMAL NOT NULL DEFAULT '0.5',
-	"environmental" DECIMAL NOT NULL DEFAULT '0.5',
-	"human_rights" DECIMAL NOT NULL DEFAULT '0.5',
+	"transparency" DECIMAL NOT NULL DEFAULT '5',
+	"environmental" DECIMAL NOT NULL DEFAULT '5',
+	"human_rights" DECIMAL NOT NULL DEFAULT '5',
 	CONSTRAINT "user_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
+
 
 CREATE TABLE "value" (
 	"id" serial NOT NULL,
@@ -24,6 +24,7 @@ CREATE TABLE "value" (
 ) WITH (
   OIDS=FALSE
 );
+
 
 CREATE TABLE "preference" (
 	"id" serial NOT NULL,
@@ -41,21 +42,48 @@ CREATE TABLE "score_rule" (
 	"result" varchar(255) NOT NULL,
 	"points" integer NOT NULL,
 	"industry" varchar(255) NOT NULL,
+	"value_id" integer NOT NULL,
 	CONSTRAINT "score_rule_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
+
 
 CREATE TABLE "membership_rule" (
 	"id" serial NOT NULL,
 	"organization" varchar(255) NOT NULL,
 	"points" integer NOT NULL,
 	"industry" varchar(255) NOT NULL,
+	"value_id" integer NOT NULL,
 	CONSTRAINT "membership_rule_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
 
 
+CREATE TABLE "comments" (
+	"id" serial NOT NULL,
+	"comment" varchar(1000) NOT NULL,
+	"user_id" integer NOT NULL,
+	CONSTRAINT "comments_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+CREATE TABLE "classification_code" (
+	"id" serial NOT NULL,
+	"code" integer NOT NULL UNIQUE,
+	"description" varchar NOT NULL,
+	CONSTRAINT "classification_code_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
 ALTER TABLE "preference" ADD CONSTRAINT "preference_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("id");
 ALTER TABLE "preference" ADD CONSTRAINT "preference_fk1" FOREIGN KEY ("value_id") REFERENCES "value"("id");
+ALTER TABLE "score_rule" ADD CONSTRAINT "score_rule_fk0" FOREIGN KEY ("value_id") REFERENCES "value"("id");
+ALTER TABLE "membership_rule" ADD CONSTRAINT "membership_rule_fk0" FOREIGN KEY ("value_id") REFERENCES "value"("id");
+ALTER TABLE "comments" ADD CONSTRAINT "comments_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("id");

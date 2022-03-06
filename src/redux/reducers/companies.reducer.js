123ef6e4f1d97c3d54b80efa6090a9009bd40d3c
James Petran <jm.ptrn@gmail.com>
2022-03-05 22:29:34 -0600

    companies reducer created & sourced

---
+++ src/redux/reducers/companies.reducer.js
@@ -0,0 +1,12 @@
+const companiesReducer = (state = {}, action) => {
+    switch (action.type) {
+        case 'SET_COMPANIES':
+            return action.payload;
+        default:
+            return state;
+    }
+};
+
+  // messages will be on the redux state at:
+  // state.messages
+export default companiesReducer;

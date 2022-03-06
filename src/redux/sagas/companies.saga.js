32bc12067644c425f73eed734d4c7df0792ce4a7
James Petran <jm.ptrn@gmail.com>
2022-03-05 22:37:45 -0600

    created fetch companies fx in companies saga ;

--- src/redux/sagas/companies.saga.js
+++ src/redux/sagas/companies.saga.js
@@ -1,9 +1,20 @@
 import axios from 'axios';
 import { put, takeLatest } from 'redux-saga/effects';
 
+function* fetchCompanies(){
+
+    return null
+}
+
+function* fetchActiveCompany(){
+
+    return null
+}
+
+
 function* companiesSaga() {
-    yield takeLatest('FETCH_COMPANIES', null);
-    yield takeLatest('FETCH_ACTIVE_COMPANY', null);
+    yield takeLatest('FETCH_COMPANIES', fetchCompanies);
+    yield takeLatest('FETCH_ACTIVE_COMPANY', fetchActiveCompany);
   }
   
   export default companiesSaga;

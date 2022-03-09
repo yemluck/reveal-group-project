f90f826e40631ed5e8963efd87b2d17121f2be5d
jeremyacoop <63517273+jeremyacoop@users.noreply.github.com>
2022-03-09 15:29:54 -0600

    Wrote basic display template

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,8 +1,63 @@
 const Users = () => {
+
+    /* 
+        tables pulling from: users, value, preference
+            user preferences:
+    */
+
     return(
+        <>
         <div>
             Admin can see user details
         </div>
+        <div>
+            <h3>Average User Preferences</h3>
+        </div>
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>
+                            Transparency
+                        </th>
+                        <th>
+                            Environment
+                        </th>
+                        <th>
+                            Human Rights
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>data</td>
+                        <td>data</td>
+                        <td>data</td>
+                    </tr>
+                </tbody>
+                <thead>
+                    <tr>
+                        <th>Total Users</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                    </tr>
+                </tbody>
+                <thead>
+                    <tr>
+                        <th>User Emails</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </>
     );
 }
 

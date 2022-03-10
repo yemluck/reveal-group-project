cc0ddf3a8274c92bb3e8fb2b718a9379d2f2460f
James Petran <jm.ptrn@gmail.com>
2022-03-10 15:15:15 -0600

    commented out changes & features until the reducer is all ready

--- src/components/CompanyDetails/CompanyDetails.jsx
+++ src/components/CompanyDetails/CompanyDetails.jsx
@@ -9,33 +9,54 @@ function CompanyDetails() {
     //console.log(params);
     const dispatch = useDispatch();
     const details = useSelector(store => store.companyDetails)
-    const companyData = useSelector(store => store.companyData)
-    const membershipRules = useSelector(store => store.membershipRules)
-    const scoreRules = useSelector(store => store.scoreRules)
+
+    // rename these as necessary when the reducers are all done & populated
+    // const companyData = useSelector(store => store.companyData)
+    // const membershipRules = useSelector(store => store.membershipRules)
+    // const scoreRules = useSelector(store => store.scoreRules)
+    
     const keys = Object.keys(details);
     //   console.log('details keys:',keys[0]);
     const abc = keys[0]
     //   console.log('type of key', typeof keys[0]); 
     //   console.log('details from store', details);
+    let totalScore = {
+        transparencyTotal: 0,
+        transparencyScore: 0,
+        humanRightsTotal: 0,
+        humanRightsScore: 0,
+        environmentTotal: 0,
+        environmentScore: 0,
+    }
 
-    const pointTotal = score({ membershipRules, scoreRules}, companyData);
+    
 
-    useEffect(() => {
+    useEffect(async () => {
         // dispatch to fetch description
         dispatch({
             type: 'FETCH_COMPANY_DETAILS',
             payload: companyName
         });
         // dispatch to fetch data
-        dispatch({
+        await dispatch({
             type: 'FETCH_COMPANY_DATA',
             payload: companyName
         });
+        
+        // dispatch to receive rules
+        // await dispatch({
+        //     type: 'FETCH_RULES',
+        //     payload: companyName
+        // });
+
         // add viewed company as current active company
         dispatch({
             type: 'SET_ACTIVE_COMPANY',
             payload: companyName
-        })
+        });
+
+        // reenable this to run it
+        // await totalScore = score({ membershipRules, scoreRules}, companyData);
     }, [companyName])
 
 
@@ -45,9 +66,9 @@ function CompanyDetails() {
                 <h2>Company Details</h2>
                 <h3>{companyName} </h3>
                 <p>{details[abc].extract}</p>
-                <p>Transparency: {pointTotal.transparencyScore} / {pointTotal.transparencyTotal}: {pointTotal.transparencyScore / pointTotal.transparencyTotal}</p>
-                <p>Environment: {pointTotal.environmentScore} / {pointTotal.environmentTotal}: {pointTotal.environmentScore / pointTotal.environmentTotal}</p>
-                <p>Human Rights: {pointTotal.humanRightsScore} / {pointTotal.humanRightsTotal}: {pointTotal.humanRightsScore / pointTotal.humanRightsTotal}</p>
+                <p>Transparency: {totalScore.transparencyScore} / {totalScore.transparencyTotal}: {totalScore.transparencyScore / totalScore.transparencyTotal}</p>
+                <p>Environment: {totalScore.environmentScore} / {totalScore.environmentTotal}: {totalScore.environmentScore / totalScore.environmentTotal}</p>
+                <p>Human Rights: {totalScore.humanRightsScore} / {totalScore.humanRightsTotal}: {totalScore.humanRightsScore / totalScore.humanRightsTotal}</p>
                 <Link to="/companies"><button> Back </button></Link>
             </div>
         </div>

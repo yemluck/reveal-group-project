296c218544eaaf88bad5a6a3302b6d6b12900c49
jeremyacoop <63517273+jeremyacoop@users.noreply.github.com>
2022-03-15 09:37:20 -0500

    Added more comments

--- src/redux/reducers/userData.reducer.js
+++ src/redux/reducers/userData.reducer.js
@@ -1,29 +1,30 @@
 import { combineReducers } from "redux";
 
+// provides list of averages for all user preferences 
+// in each value category
 const userPreferencesReducer = (state = [], action) => {
     switch (action.type) {
         case 'SET_USER_PREFERENCES':
-            // console.log(action);
             return action.payload;
         default:
             return state;
     }
-};
+};// end userPreferencesReducer
 
-// provides
+// provides list of all user email addresses, 
+// and the total user count
 const userEmailsReducer = (state = [], action) => {
     switch (action.type) {
         case 'SET_USER_EMAILS':
-            // console.log(action);
             return action.payload;
         default:
             return state;
     }
-};
+};// end userEmailsReducer
 
 const userData = combineReducers({
     userPreferencesReducer,
     userEmailsReducer,
-}) 
+})// end userData  
 
 export default userData;

64bc0535ae90708eea11137c93189404b2ce263e
Chris Agbolade <yemluck@gmail.com>
2022-03-18 14:06:04 -0500

    fixed the more info icon on company details page

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 // This is one of our simplest components
 // It doesn't have local state,
@@ -12,6 +13,7 @@ function AboutPage() {
         <p>Reveal is a nonprofit starting, aiming to put the power of investigation, reporting and honesty into the hands 
         of the people. Reveal aims to uncover the good, the bad, and the ugly about corporations and organizations in the areas of Human Rights, Transparency, and the Environment.</p>
       </div>
+      <Link to="/companies"><button>OK</button></Link>
     </div>
   );
 }
